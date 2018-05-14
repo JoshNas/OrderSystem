995167eb7488aca783b03d8a5891fbcad6915a37
JoshNas <36458322+JoshNas@users.noreply.github.com>
2018-05-13 21:52:01 -0400

    Add files via upload

--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import database_functions as dbf
 import datetime as dt
 import os
+import csv
 
 
 class ManagerApplication(object):
@@ -54,45 +55,45 @@ class ManagerApplication(object):
         self.keypad = tk.Canvas()
         self.keypad.grid(row=1, column=0, sticky='nsew')
 
-        tk.Label(self.keypad, text='Month').grid(row=0, column=3, sticky='nsew')
+        tk.Label(self.keypad, text='Month').grid(row=1, column=3, sticky='nsew')
         self.month = tk.StringVar(self.root)
         months = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
         self.month.set(1)  # set the default option
-        self.month_selector = tk.OptionMenu(self.keypad, self.month, *months).grid(row=1, column=3, sticky='new')
+        self.month_selector = tk.OptionMenu(self.keypad, self.month, *months).grid(row=2, column=3, sticky='new')
 
-        tk.Label(self.keypad, text='Day').grid(row=0, column=4, sticky='nsew')
+        tk.Label(self.keypad, text='Day').grid(row=1, column=4, sticky='nsew')
         self.day = tk.StringVar(self.root)
         days = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
                 29, 30, 31}
         self.day.set(1)
-        self.day_selector = tk.OptionMenu(self.keypad, self.day, *days).grid(row=1, column=4, sticky='new')
+        self.day_selector = tk.OptionMenu(self.keypad, self.day, *days).grid(row=2, column=4, sticky='new')
 
-        tk.Label(self.keypad, text='Year').grid(row=0, column=5, sticky='nsew')
+        tk.Label(self.keypad, text='Year').grid(row=1, column=5, sticky='nsew')
         self.year = tk.StringVar(self.root)
         years = {18, 19, 20}
         self.year.set(18)
-        self.year_selector = tk.OptionMenu(self.keypad, self.year, *years).grid(row=1, column=5, sticky='new')
+        self.year_selector = tk.OptionMenu(self.keypad, self.year, *years).grid(row=2, column=5, sticky='new')
 
-        tk.Label(self.keypad, text='Month').grid(row=2, column=3, sticky='nsew')
+        tk.Label(self.keypad, text='Month').grid(row=3, column=3, sticky='nsew')
         self.month2 = tk.StringVar(self.root)
         self.month2.set(1)  # set the default option
-        self.month_selector = tk.OptionMenu(self.keypad, self.month2, *months).grid(row=3, column=3, sticky='new')
+        self.month_selector = tk.OptionMenu(self.keypad, self.month2, *months).grid(row=4, column=3, sticky='new')
 
-        tk.Label(self.keypad, text='Day').grid(row=2, column=4, sticky='nsew')
+        tk.Label(self.keypad, text='Day').grid(row=3, column=4, sticky='nsew')
         self.day2 = tk.StringVar(self.root)
         self.day2.set(1)
-        self.day_selector = tk.OptionMenu(self.keypad, self.day2, *days).grid(row=3, column=4, sticky='new')
+        self.day_selector = tk.OptionMenu(self.keypad, self.day2, *days).grid(row=4, column=4, sticky='new')
 
-        tk.Label(self.keypad, text='Year').grid(row=2, column=5, sticky='nsew')
+        tk.Label(self.keypad, text='Year').grid(row=3, column=5, sticky='nsew')
         self.year2 = tk.StringVar(self.root)
         self.year2.set(18)
-        self.year_selector = tk.OptionMenu(self.keypad, self.year2, *years).grid(row=3, column=5, sticky='new')
+        self.year_selector = tk.OptionMenu(self.keypad, self.year2, *years).grid(row=4, column=5, sticky='new')
 
-        tk.Label(self.keypad, text='Tip Out %').grid(row=3, column=1, sticky='nsew')
+        tk.Label(self.keypad, text='Tip Out %').grid(row=3, column=0, sticky='nsew')
         self.tipout = tk.StringVar(self.root)
-        tipout_percent = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+        tipout_percent = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
         self.tipout.set(8)
-        self.year_selector = tk.OptionMenu(self.keypad, self.tipout, *tipout_percent).grid(row=4, column=1, sticky='new')
+        self.year_selector = tk.OptionMenu(self.keypad, self.tipout, *tipout_percent).grid(row=4, column=0, sticky='new')
 
         self.display_window = tk.Canvas()
         self.display_window.grid(row=1, column=1, sticky='nsew')
@@ -101,13 +102,14 @@ class ManagerApplication(object):
         self.dis_window.grid(row=1, column=0)
 
         """Buttons"""
-        tk.Button(self.entry_window, text='Update Menu', command=self.update_menu).grid(row=2, column=0, sticky='ne')
+        tk.Button(self.keypad, text='Update Menu', command=self.update_menu).grid(row=0, column=0, sticky='w')
+        tk.Button(self.keypad, text='Display Menu', command=self.display_menu).grid(row=0, column=1, sticky='w')
+        tk.Button(self.keypad, text='Delete Item', command=self.delete_from_menu).grid(row=0, column=2, sticky='w')
         tk.Button(self.selector_window, text='Set', command=self.set_num_per_rows).grid(row=1, column=3, sticky='nsew')
-        tk.Button(self.keypad, text='Totals', command=self.get_totals).grid(row=1, column=0, sticky='ne')
-        tk.Button(self.keypad, text='Total Day', command=self.manager_total_day).grid(row=1, column=1, sticky='ne')
-        tk.Button(self.keypad, text='Total Date', command=self.date_totals).grid(row=1, column=2, sticky='ne')
-        tk.Button(self.keypad, text='Date Range', command=self.date_range_totals).grid(row=3, column=2, sticky='ne')
-    tip_out_percent = .08
+        tk.Button(self.keypad, text='Totals', command=self.get_totals).grid(row=2, column=0, sticky='ne')
+        tk.Button(self.keypad, text='Total Day', command=self.manager_total_day).grid(row=2, column=1, sticky='ne')
+        tk.Button(self.keypad, text='Total Date', command=self.date_totals).grid(row=2, column=2, sticky='ne')
+        tk.Button(self.keypad, text='Date Range', command=self.date_range_totals).grid(row=4, column=2, sticky='ne')
 
     def get_totals(self):
         conn = dbf.create_connection('totals.sqlite3')
@@ -249,7 +251,7 @@ class ManagerApplication(object):
 
         conn.close()
 
-    def update_menu(self,):
+    def update_menu(self):
         addition = ''
 
         if len(self.item_entry.get()) > 0 and len(self.price_entry.get()):
@@ -271,6 +273,35 @@ class ManagerApplication(object):
             with open(menu, 'a') as f:
                 addition = '\n{}'.format(addition)
                 f.write(addition)
+
+    def delete_from_menu(self):
+        selections = self.dis_window.selection_get()
+        items = selections.split('\n')
+        menu = self.menus.get()
+        new_menu = []
+
+        with open(menu, 'r') as m:
+            m.seek(0)
+            for row in m:
+                r = row.strip('\n')
+                if r not in items:
+                    new_menu.append(r)
+
+        with open(menu, 'w') as m:
+            first = True
+            for i in new_menu:
+                if first:
+                    m.write(i)
+                    first = False
+                else:
+                    m.write('\n{}'.format(i))
+
+    def display_menu(self):
+        self.dis_window.delete(1.0, 'end')
+        menu = self.menus.get()
+        with open(menu) as m:
+            for row in m:
+                self.dis_window.insert('end', row)
 
     def set_num_per_rows(self):
         with open('perRow.csv', 'w') as f:
